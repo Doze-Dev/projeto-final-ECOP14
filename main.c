a7f81fe4f4066b1f5c7df764ab97464ccfa796db
Felipe Brito <felipef_brito@outlook.com>
2021-11-18 21:15:43 -0300

    cronometro

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include "keypad.h"
 #include "config.h"
 #include "serial.h"
+#include "bits.h"
 
 #define L_ON	0x0F
 #define L_OFF	0x08
@@ -13,17 +14,144 @@
 #define L_L2	0xC0
 
 
+
+void crono(int t){
+    int min;
+    
+    min = t/60;
+    
+    t = t*100;
+    
+    ssdInit();
+    
+    TRISD = 0x00;
+
+    
+    while(t != 0) {
+        
+        ssdDigit(((min / 1) % 10), 1);
+        ssdDigit(((min / 10) % 10), 0);
+        
+        ssdDigit(((t / 100) % 10), 3); 
+        ssdDigit(((t / 1000) % 6), 2); 
+        
+        if(((t/1000)%6 == 0)&&((t/100)%10 == 0)&&((t/10)%10 == 0)&&((t/1)%10 == 0)){
+            min--;
+        }
+          
+        
+        t--;
+        ssdUpdate();
+        atraso_ms(1);
+    }
+    
+    PORTD = 0x0A;
+}
+
 void portempolivre(){
+    unsigned int tecla = 16;
+    char tec = 0;
+    char min = 0;
+    int conv, tempo;
+    
     lcdCommand(L_CLR);
     lcdCommand(L_L1);
     lcdString("MODO TEMPO");
+    atraso_ms(200);
+    lcdCommand(L_CLR);
+    lcdCommand(L_L1);
+    lcdString("Altere o Tempo");
+    
+    serial_tx_str(" Altere o tempo usando as teclas:\r\n");
+    
+    while(tec == 0){
+        kpDebounce();
+        
+        
+        if(kpRead() != tecla){
+            tecla = kpRead();
+            if(bitTst(tecla,0)){
+                min++;
+                serial_tx(min);
+               
+                
+            }
+            if(bitTst(tecla,2)){
+                min--;
+                serial_tx(min);
+                
+            }
+            if(bitTst(tecla,4)){
+                tec = 1;
+                serial_tx_str(" Tempo alterado\r\n");
+            }  
+        }
+        
+        
+        
+    }
+    
+    conv = min - '0';
+    tempo = conv * 60;
+    
+    crono(tempo);
 }
 
 
 void porpeso(){
+    unsigned char peso;
+    int i;
+    
     lcdCommand(L_CLR);
     lcdCommand(L_L1);
     lcdString("MODO PESO");
+    atraso_ms(200);
+    lcdCommand(L_CLR);
+    lcdCommand(L_L1);
+    lcdString("(1)100g  (2)300g");
+    lcdCommand(L_L2);
+    lcdString("(3)700g");
+    
+    serial_tx_str(" Digite a opcao de peso:\r\n");
+    
+    for (i = 0; i < 2; i++) {
+        
+        peso = serial_rx(2000);
+        serial_tx(peso);
+        
+        switch(peso){
+            case '1':
+                lcdCommand(L_CLR);
+                lcdCommand(L_L1);
+                lcdString("Descongelando");
+                lcdCommand(L_L2);
+                lcdString("100g");
+                crono(150);
+                //2:30
+                break;
+            case '2':
+                lcdCommand(L_CLR);
+                lcdCommand(L_L1);
+                lcdString("Descongelando");
+                lcdCommand(L_L2);
+                lcdString("300g");
+                crono(250);
+                //4:10
+                break;
+            case '3':
+                lcdCommand(L_CLR);
+                lcdCommand(L_L1);
+                lcdString("Descongelando");
+                lcdCommand(L_L2);
+                lcdString("700g");
+                crono(480);
+                //10:00
+                break;
+                        
+        }
+        
+    }
+    
 }
 
 void descongela(){
@@ -33,7 +161,7 @@ void descongela(){
     lcdCommand(L_CLR);
     lcdCommand(L_L1);
     lcdString("MODO DESCONGELAR");
-    atraso_ms(150);
+    atraso_ms(200);
     lcdCommand(L_CLR);
     lcdCommand(L_L1);
     lcdString("(1)Por Peso");
@@ -64,7 +192,7 @@ void descongela(){
 
 void main(void) {
     unsigned char i;
-    unsigned char op, data[12];
+    unsigned char op;
     
     TRISA = 0xC3;
     TRISB = 0x03;
@@ -75,8 +203,9 @@ void main(void) {
     
     lcdInit();
     serial_init();
-    ssdInit();
     kpInit();
+    
+    
     
     
     TRISCbits.TRISC7 = 1; 
@@ -92,12 +221,8 @@ void main(void) {
     serial_tx_str(" Digite a opcao:\r\n");
     
     for (i = 0; i < 2; i++) {
-        //if (!(i % 16)) {
-           // serial_tx_str("\r\n");
-        //}
         
         op = serial_rx(2000);
-        data[i] = op;
         serial_tx(op);
         
         switch(op){
@@ -105,7 +230,7 @@ void main(void) {
                 lcdCommand(L_CLR);
                 lcdCommand(L_L1);
                 lcdString("MODO PIPOCA");
-                //crono();
+                crono(140);
                 break;
             case '2':
                 descongela();
